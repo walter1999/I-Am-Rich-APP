9f8adace915e67c3c5c27a3d48693471285c369d
Walter Galdamez <galdamez.w@husky.neu.edu>
2019-05-02 20:53:12 -0400

    This is my first ios app

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by walter galdamez on 5/2/19.
+//  Copyright © 2019 walter galdamez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
